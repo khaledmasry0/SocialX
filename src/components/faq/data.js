5640741144f4294c3e484624cb037625d114dba5
Masry <khaled.m00@yahoo.com>
2022-11-21 21:18:30 +0200

    first commit

---
+++ src/components/faq/data.js
@@ -0,0 +1,17 @@
+export const questions = [
+  {
+    id: 1,
+    title: "Can I join this ",
+    answer: "Yes, you can ",
+  },
+  {
+    id: 2,
+    title: "How can I get started with SocialX?",
+    answer: "you can get started with socialX by contact us ",
+  },
+  {
+    id: 3,
+    title: "Is there a refund policy?",
+    answer: "yes there is a refund policy by default",
+  },
+];
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,63 @@
+import React from "react";
+import "./Footer.css";
+import logo from "../../assets/logo.png";
+import {
+  FaMapMarkerAlt,
+  FaPhoneAlt,
+  FaFax,
+  FaEnvelope,
+  FaGlobe,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <section id="footer">
+      <div className="container footer">
+        <div className="footer-box">
+          <h4>Useful Links</h4>
+          <div className="footer-links">
+            <a href="#">&bull; Support</a>
+            <a href="#">&bull; About</a>
+            <a href="#">&bull; Learn</a>
+            <a href="#">&bull; Hosting</a>
+            <a href="#">&bull; Messenger</a>
+          </div>
+        </div>
+        <div className="footer-box">
+          <h4>Support</h4>
+          <div className="footer-links">
+            <a href="#">&bull; Support</a>
+            <a href="#">&bull; About</a>
+            <a href="#">&bull; Learn</a>
+            <a href="#">&bull; Hosting</a>
+            <a href="#">&bull; Messenger</a>
+          </div>
+        </div>
+        <div className="footer-box">
+          <h4>Contact Us</h4>
+          <div className="footer-contact u-text-small">
+            <p>
+              <FaMapMarkerAlt /> &nbsp; Address: United States.
+            </p>
+            <p>
+              <FaPhoneAlt /> &nbsp; Phone: 0123456789.
+            </p>
+            <p>
+              <FaFax /> &nbsp; Fax: +12342762178
+            </p>
+            <p>
+              <FaEnvelope /> &nbsp; Email: khaledmohamed3360
+            </p>
+          </div>
+        </div>
+        <div className="footer-box">
+          <img src={logo} alt="logo" />
+          <p className="u-text-small para">&copy; Copyright 2022.</p>
+          <p className="tanp">This tamplate is made with &lt;3 By Khaled</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
